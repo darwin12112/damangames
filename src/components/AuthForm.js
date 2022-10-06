c9d54f2bc8f0948ccaf0f7f0be0139db691b8f7a
darwin12112 <70789419+darwin12112@users.noreply.github.com>
2022-10-06 17:39:28 +0530

    Update AuthForm.js

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -451,7 +451,7 @@ sendOTP=()=>{
                       </a>
                       <br />
                       <br />
-                      <a href="https://t.me/colortrading247">
+                      <a href="https://t.me/damangames101">
                         Contact US
                       </a>
 
