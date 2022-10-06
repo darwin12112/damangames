cac1290a68e7e85761388e37d25721c0a1edb683
darwin12112 <70789419+darwin12112@users.noreply.github.com>
2022-10-06 17:38:07 +0530

    Update AdminRewardPage.js

--- src/pages/AdminRewardPage.js
+++ src/pages/AdminRewardPage.js
@@ -135,7 +135,7 @@ const AdminReward = (props) => {
           (list && list.length>0) ?
           list.map((ele,key)=>(
           <div className={'form-control'} key={key} style={{ marginBottom: '0rem',height:'100px'}}>
-            <span>https://www.predictionmall.com/api/reward/{ele._id}</span><br />
+            <span>https://www.damangames.co/api/reward/{ele._id}</span><br />
             <span>₹ {ele.money} </span><br />
             <span>Phone : {ele.userphone}</span>
             {' '}&nbsp;
